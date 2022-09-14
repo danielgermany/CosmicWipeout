a1369d586cb730fc7fb1ee203cc4c4a1e85e87f5
danielgermany <dgermany333@gmail.com>
2022-09-14 15:44:05 -0400

    Moved score calc to Player class

--- CosmicWipeout.java
+++ CosmicWipeout.java
@@ -1,39 +1,11 @@
+
 public class CosmicWipeout {
     public static void main(String[] args) {
         DiceSet set = new DiceSet(5);
         set.rollSet();
         Player Player1 = new Player(set.dieInfo);
-
-    }
-
-    public int CalculateScore(String[] set) {
-        int score = 0;
-        int counter_5 = 0;
-        int counter_10 = 0;
-        int scoringdie = 0;
-        for (int i = 0; i < set.length; i++) {
-            switch (set[i]) {
-                case "5":
-                    score += 5;
-                    counter_5++;
-                case "10":
-                    score += 10;
-                    counter_10++;
-            }
-
-        }
-        if (counter_5 == 3) {
-            score += 35;
-        }
-        if (counter_10 == 3) {
-            score += 70;
-        }
-        scoringdie = counter_10 + counter_5;
-        int diceleft = 6 - scoringdie;
-
-        if (diceleft != 0) {
-
-        }
-        return score;
+        Player1.CalculateScore();
+        System.out.println(Player1.setInfo);
+        System.out.println(Player1.Score);
     }
 }
--- Player.java
+++ Player.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Player {
     int Score;
@@ -9,5 +10,16 @@ public class Player {
         this.Score = 0;
         this.Round = 0;
         this.setInfo = setInfo;
+    }
+
+    public void CalculateScore() {
+        int score = 0;
+
+        int freq_5 = Collections.frequency(this.setInfo, "5");
+        int freq_10 = Collections.frequency(this.setInfo, "10");
+
+        score = (freq_5 * 5) + (freq_10 * 10);
+
+        this.Score = score;
     }
 }
